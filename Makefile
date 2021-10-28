5c8dda0994a1aef0ee35008cf16fec9cc446052d
dronperminov <dronperminov@gmail.com>
2021-10-28 11:31:58 +0300

    add test script

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ FLAGS=-O3 -Wall -pedantic -fopenmp -std=c++11
 MPI_COMPILER=mpixlC
 MPI_FLAGS=-O3 -Wall -pedantic -fopenmp -std=c++11
 
+N=256
+K=2000
+
 all: main main-mpi
 
 main: main.cpp
@@ -12,6 +15,17 @@ main: main.cpp
 main-mpi: main_mpi.cpp
 	$(MPI_COMPILER) $(MPI_FLAGS) main_mpi.cpp -o main_mpi
 
+test-mpi: test_mpi.cpp
+	$(MPI_COMPILER) $(MPI_FLAGS) test_mpi.cpp -o test_mpi
+
 submit-polus: main-mpi
-	rm -rf output.txt error.txt
-	mpisubmit.pl -p 5 --stdout output.txt --stderr error.txt ./main_mpi
+	mpisubmit.pl -p 16 --stdout stdout.txt --stderr error.txt ./main_mpi -- -d -o output.txt -N $(N) -K $(K)
+
+submit-polus-test: test-mpi
+	for N in 128 256 512; do \
+		for p in 1 2 4 8 10 16 20 32 40 64; do \
+			for L in 1 3.14159265358979; do \
+				mpisubmit.pl -p $$p -w 00:20 --stdout /dev/null --stderr /dev/null ./test_mpi -- test_L$$L\_$$N.txt $$N $$L ; \
+			done \
+		done \
+	done
---
+++ test_mpi.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <mpi.h>
+
+#include "MPIHyperbolicEquationSolver.hpp"
+
+using namespace std;
+
+void InitMPI(int &rank, int &size) {
+    if (MPI_Init(NULL, NULL) != MPI_SUCCESS) {
+        MPI_Abort(MPI_COMM_WORLD, -1);
+        throw "MPI_Init failed";
+    }
+
+    if (MPI_Comm_size(MPI_COMM_WORLD, &size) != MPI_SUCCESS) {
+        MPI_Abort(MPI_COMM_WORLD, -1);
+        throw "MPI_Comm_size failed";
+    }
+
+    if (MPI_Comm_rank(MPI_COMM_WORLD, &rank) != MPI_SUCCESS) {
+        MPI_Abort(MPI_COMM_WORLD, -1);
+        throw "MPI_Comm_rank failed";
+    }
+}
+
+bool IsExists(const char *path) {
+    ifstream fin(path);
+
+    if (!fin)
+        return false;
+
+    fin.close();
+    return true;
+}
+
+int main(int argc, char **argv) {
+    int rank, size;
+
+    try {
+        InitMPI(rank, size);
+    }
+    catch (const char *error) {
+        return -1;
+    }
+
+    int N = atoi(argv[2]);
+    double L = atof(argv[3]);
+
+    if (!IsExists(argv[1])) {
+        ofstream fout(argv[1]);
+        fout << "### Lx = Ly = Lz = " << L << ", N = " << N << ", K = " << K << endl << endl;
+        fout << "| Число MPI процессов (P) | Время решения (с) | Ускорение | Погрешность |" << endl;
+        fout << "|                     :-: |               :-: |       :-: |         :-: |" << endl;
+        fout.close();
+    }
+
+    double T = 1;
+    int K = 2000;
+    int steps = 20;
+    int loops = 5;
+
+    BoundaryConditionTypes bt = {
+        BoundaryConditionType::FirstKind,
+        BoundaryConditionType::PeriodicAnalytical,
+        BoundaryConditionType::FirstKind
+    };
+
+    double t0 = MPI_Wtime();
+    double error = 0;
+
+    for (int loop = 0; loop < loops; loop++) {
+        MPIHyperbolicEquationSolver solver({ L, L, L }, T, N, K, bt, rank, size);
+        error += solver.Solve(steps, NULL, NULL, NULL, true);
+    }
+
+    double t1 = MPI_Wtime();
+    double delta = t1 - t0;
+    double maxTime;
+
+    MPI_Reduce(&delta, &maxTime, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        ofstream fout(argv[1], ios::app);
+        fout << "| " << setw(23) << size;
+        fout << " | " << setw(17) << (maxTime / loops);
+        fout << " | " << "         ";
+        fout << " | " << setw(11) << (error / loops);
+        fout << " |" << endl;
+        fout.close();
+    }
+
+    MPI_Finalize();
+}
